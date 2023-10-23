f71d54aa057338fde8717692a67a7cd86d4ed13c
Tarannum khan <125120619+tarannum3824@users.noreply.github.com>
2023-10-23 23:39:21 +0530

    Add files via upload

---
+++ Day 15_valid parenthesis.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+          Stack<Character> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char openBracket = stack.pop();
+                if ((c == ')' && openBracket != '(') || (c == '}' && openBracket != '{') || (c == ']' && openBracket != '[')) {
+                    return false;
+                }
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
