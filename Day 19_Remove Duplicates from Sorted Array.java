9a2167f6e6fc5f60e608a2131c4301057b2f2d07
Tarannum khan <125120619+tarannum3824@users.noreply.github.com>
2023-10-27 19:39:35 +0530

    Add files via upload

---
+++ Day 19_Remove Duplicates from Sorted Array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        
+        int uniqueCount = 1; // At least one unique element (the first element)
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[uniqueCount] = nums[i];
+                uniqueCount++;
+            }
+        }
+        
+        return uniqueCount;
+    }
+}
