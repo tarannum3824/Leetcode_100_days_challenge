726ea703d3e10b134952d6a22b2c465300a59294
Tarannum khan <125120619+tarannum3824@users.noreply.github.com>
2023-11-08 22:44:52 +0530

    Add files via upload

---
+++ Day 31_remove duplicate from sorted list.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                // Remove the duplicate by updating the next pointer
+                current.next = current.next.next;
+            } else {
+                // Move to the next distinct element
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+}
