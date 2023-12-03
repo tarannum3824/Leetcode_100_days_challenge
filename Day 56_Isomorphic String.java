0931ec6be5099196d0096abb2be9dd343117f2d1
Tarannum khan <125120619+tarannum3824@users.noreply.github.com>
2023-12-03 23:16:59 +0530

    Create Day 56_Isomorphic String.java

---
+++ Day 56_Isomorphic String.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+         if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Character> sToT = new HashMap<>();
+        HashMap<Character, Character> tToS = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char charS = s.charAt(i);
+            char charT = t.charAt(i);
+
+            // Check if the mapping from s to t is consistent
+            if (sToT.containsKey(charS)) {
+                if (sToT.get(charS) != charT) {
+                    return false;
+                }
+            } else {
+                sToT.put(charS, charT);
+            }
+
+            // Check if the mapping from t to s is consistent
+            if (tToS.containsKey(charT)) {
+                if (tToS.get(charT) != charS) {
+                    return false;
+                }
+            } else {
+                tToS.put(charT, charS);
+            }
+        }
+
+        return true;
+    }
+}
