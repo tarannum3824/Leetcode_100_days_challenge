313e7bd7d8bd8ffe74370784b31b6fa022f23376
Tarannum khan <125120619+tarannum3824@users.noreply.github.com>
2023-12-10 13:42:49 +0530

    Add files via upload

---
+++ Day 63_First Missing Positive.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+
+        // Step 1: Place each number in its correct position
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                // Swap nums[i] with nums[nums[i] - 1]
+                int temp = nums[i];
+                nums[i] = nums[temp - 1];
+                nums[temp - 1] = temp;
+            }
+        }
+
+        // Step 2: Find the first missing positive integer
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        // If all positive integers from 1 to n are present, return n + 1
+        return n + 1;
+    }
+}
