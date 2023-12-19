495580aa40855a3293abff381912f55d94db3bd1
Tarannum khan <125120619+tarannum3824@users.noreply.github.com>
2023-12-19 10:28:46 +0530

    Add files via upload

---
+++ Day 72_Jump Game II.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int jump(int[] nums) {
+        int n = nums.length;
+        if (n == 1) {
+            return 0; // Already at the last index
+        }
+
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
+
+                if (currentEnd >= n - 1) {
+                    break; // Reached or surpassed the last index
+                }
+            }
+        }
+
+        return jumps;
+    }
+}
