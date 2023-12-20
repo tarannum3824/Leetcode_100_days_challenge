0781ecc29e51728275b2223fbef8efea6ce3e7dc
Tarannum khan <125120619+tarannum3824@users.noreply.github.com>
2023-12-20 21:44:44 +0530

    Add files via upload

---
+++ Day 73_Maximum Subarray.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        return maxSubArrayHelper(nums, 0, nums.length - 1);
+    }
+
+    private int maxSubArrayHelper(int[] nums, int left, int right) {
+        if (left == right) {
+            return nums[left];
+        }
+
+        int mid = left + (right - left) / 2;
+
+        int leftMax = maxSubArrayHelper(nums, left, mid);
+        int rightMax = maxSubArrayHelper(nums, mid + 1, right);
+        int crossMax = maxCrossingSum(nums, left, mid, right);
+
+        return Math.max(Math.max(leftMax, rightMax), crossMax);
+    }
+
+    private int maxCrossingSum(int[] nums, int left, int mid, int right) {
+        int leftSum = Integer.MIN_VALUE;
+        int sum = 0;
+
+        for (int i = mid; i >= left; i--) {
+            sum += nums[i];
+            leftSum = Math.max(leftSum, sum);
+        }
+
+        int rightSum = Integer.MIN_VALUE;
+        sum = 0;
+
+        for (int i = mid + 1; i <= right; i++) {
+            sum += nums[i];
+            rightSum = Math.max(rightSum, sum);
+        }
+
+        return leftSum + rightSum;
+    }
+}
