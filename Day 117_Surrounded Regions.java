5422a0f3f171f7fbc688e4bc41a14efa4d862553
Tarannum khan <125120619+tarannum3824@users.noreply.github.com>
2024-02-02 21:11:22 +0530

    Add files via upload

---
+++ Day 117_Surrounded Regions.java
@@ -0,0 +1,46 @@
+class Solution {
+    public void solve(char[][] board) {
+        if (board == null || board.length == 0 || board[0].length == 0) {
+            return;
+        }
+
+        int m = board.length;
+        int n = board[0].length;
+
+        // Mark 'O' cells connected to the border as safe
+        for (int i = 0; i < m; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, n - 1);
+        }
+
+        for (int j = 0; j < n; j++) {
+            dfs(board, 0, j);
+            dfs(board, m - 1, j);
+        }
+
+        // Flip the remaining 'O' cells into 'X', and restore the marked safe cells
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                } else if (board[i][j] == 'S') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+
+    private void dfs(char[][] board, int i, int j) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != 'O') {
+            return;
+        }
+
+        board[i][j] = 'S'; // Mark the current 'O' as safe
+
+        // Recursively visit adjacent cells
+        dfs(board, i - 1, j);
+        dfs(board, i + 1, j);
+        dfs(board, i, j - 1);
+        dfs(board, i, j + 1);
+    }
+}
