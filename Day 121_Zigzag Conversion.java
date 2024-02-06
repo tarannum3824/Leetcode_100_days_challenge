eb89d8d7c63ea29450c10eb424674a19a3cee717
Tarannum khan <125120619+tarannum3824@users.noreply.github.com>
2024-02-06 21:26:59 +0530

    Add files via upload

---
+++ Day 121_Zigzag Conversion.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows == 1 || numRows >= s.length()) {
+            return s;
+        }
+        
+        StringBuilder[] rows = new StringBuilder[numRows];
+        for (int i = 0; i < numRows; i++) {
+            rows[i] = new StringBuilder();
+        }
+        
+        int row = 0;
+        boolean goingDown = false;
+        for (char c : s.toCharArray()) {
+            rows[row].append(c);
+            if (row == 0 || row == numRows - 1) {
+                goingDown = !goingDown;
+            }
+            row += goingDown ? 1 : -1;
+        }
+        
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder rowString : rows) {
+            result.append(rowString);
+        }
+        
+        return result.toString();
+    }
+}
